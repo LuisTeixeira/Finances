dfa2ea73fa2f8848dfd133c83d48e5920ee6509a
Luis Teixeira <teixeira13@gmail.com>
2022-05-30 19:58:35 +0200

    Remove dead code in Colors

--- app/src/main/java/com/lfmteixeira/composefinances/ui/theme/Colors.kt
+++ app/src/main/java/com/lfmteixeira/composefinances/ui/theme/Colors.kt
@@ -20,17 +20,3 @@ val FinancesColors = lightColors(
     error = errorColor,
     onError = Color.Black
 )
-
-val Red700 = Color(0xffdd0d3c)
-val Red800 = Color(0xffd00036)
-val Red900 = Color(0xffc20029)
-
-val LightColors = lightColors(
-    primary = Red700,
-    primaryVariant = Red900,
-    onPrimary = Color.White,
-    secondary = Red700,
-    secondaryVariant = Red900,
-    onSecondary = Color.White,
-    error = Red800
-)
