c0bd199d599d6cbf6ab356d61b7940f50dccaef3
Luis Teixeira <teixeira13@gmail.com>
2022-06-10 23:14:30 +0200

    Start of line chart tutorial

---
+++ app/src/main/java/com/lfmteixeira/composefinances/ui/chart/line/LineChartUtils.kt
@@ -0,0 +1,56 @@
+package com.lfmteixeira.composefinances.ui.chart.line
+
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+
+object LineChartUtils {
+    fun calculateDrawableArea(
+        xAxisDrawableArea: Rect,
+        yAxisDrawableArea: Rect,
+        size: Size,
+        offset: Float
+    ): Rect {
+        val horizontalOffset = xAxisDrawableArea.width * offset / 100f
+
+        return Rect(
+            left = yAxisDrawableArea.right + horizontalOffset,
+            top = 0f,
+            bottom = xAxisDrawableArea.top,
+            right = size.width - horizontalOffset
+        )
+    }
+
+    fun calculateXAxisDrawableArea(
+        yAxisWidth: Float,
+        labelHeight: Float,
+        size: Size
+    ): Rect {
+        val top = size.height - labelHeight
+
+        return Rect(
+            left = yAxisWidth,
+            top = top,
+            bottom = size.height,
+            right = size.width
+        )
+    }
+
+    fun Density.calculateYAxisDrawableArea(
+        xAxisLabelSize: Float,
+        size: Size
+    ): Rect {
+        // Either 50dp or 10% of the chart width.
+        val right = minOf(50.dp.toPx(), size.width * 10f / 100f)
+
+        return Rect(
+            left = 0f,
+            top = 0f,
+            bottom = size.height - xAxisLabelSize,
+            right = right
+        )
+    }
+
+
+}
